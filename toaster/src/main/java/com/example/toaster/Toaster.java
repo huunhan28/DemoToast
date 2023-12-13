c728331f09e9536bfb85889d79d6d2f9a0f7b79e
nhannh37 <nhannh37@fpt.com>
2023-12-13 10:36:00 +0700

    init

--- toaster/src/main/java/com/example/toaster/Toaster.java
+++ toaster/src/main/java/com/example/toaster/Toaster.java
@@ -1,2 +1,10 @@
-package com.example.toaster;public class Toaster {
+package com.example.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+    public static void toast(Context context, String message) {
+        Toast.makeText(context, "Hello from Toaster!" + message, Toast.LENGTH_SHORT).show();
+    }
 }
